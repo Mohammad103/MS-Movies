3523c9d870caf24169c7df3e924e7ba7b9278b4e
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2018-09-01 03:06:30 +0200

    Initial Commit

---
+++ MS Movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MS Movies
+//
+//  Created by Mohammad Shaker on 9/1/18.
+//  Copyright © 2018 Mohammad Shaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
